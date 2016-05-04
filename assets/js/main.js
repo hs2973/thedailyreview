ccf1109833d964f58073e240a690340d3c060a27
thecodeater <himal.shrestha@nyu.edu>
2016-05-04 18:22:12 +0400

    JSON request added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 var app = angular.module('mainApp', []);
+
+app.controller('NewsListCtrl', function ($scope, $http) {
+  $http.get('data.json').success(function(data) {
+    $scope.news_list = data;
+  });
+
+  $scope.orderProp = 'rank';
+});
